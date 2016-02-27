da53de48d1d754d552bc8ee416eb53c004f4931f
Ryan Magee <ryanmag1@yahoo.com>
2016-02-26 19:08:39 -0700

    Made changes to make rubocop happy, fixed a typo in my uber string
    [#114136557]

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -1,19 +1,18 @@
-#1/usr/bin/env ruby
+#!/usr/bin/env ruby
 
-
-1.upto(100) { |number|
-
+(1..100).each do |number|
   fizz = (number % 3 == 0)
   buzz = (number % 5 == 0)
 
-  puts case
-         when fizz then
-           'Fizz'
-         when buzz then
-           'Buzz'
-         when fizz && buzz then
-           'FizzBuzz'
-         else
-           number
-       end
-}
+    puts case
+           when fizz && buzz then
+             'FizzBuzz'
+           when fizz then
+             'Fizz'
+           when buzz then
+             'Buzz'
+           else
+             number
+         end
+    end
+
