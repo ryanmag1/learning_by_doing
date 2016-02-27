33fe2284afd04f797d582a6e1e60e7750227ea73
Ryan Magee <ryanmag1@yahoo.com>
2016-02-26 23:41:19 -0700

    Just added a comment that I'm pretty sure my last line of output is correct, but the method of arriving at it is not.
    [#114136539]

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -8,7 +8,7 @@ puts practice_string.upcase
 puts 'This is a \'string\' to practice with'
 puts 'The string \'' + practice_string + '\' has ' + practice_string.length.to_s + ' characters'
 puts practice_string.reverse
-puts 'practice ' * 2 + "practice!"
+puts 'practice ' * 2 + "practice!" #I'm guessing that's not the right way to do this...
 
 #reate a program called "string_practice.rb"
 #Create a variable with the string "this is a string to practice with"
