60e83a8c852d405c252419d44eec53942a24cbcd
Ryan Magee <ryanmag1@yahoo.com>
2016-03-02 08:02:16 -0700

    Initial commit of mad libs assignment
    [#114840959]

---
+++ methods_and_recursion/mad_libs.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+#
+# def mad_lib_line(start_sentence, user_input, end_sentence)
+#   puts start_sentence + user_input + end_sentence
+#    end
+
+def mad_libs
+ puts 'A vacation is when you take a trip to some ' + gets.chomp + ' place with your ' + gets.chomp + ' family. Usually you go to some place that is near a
+  /an ' + gets.chomp + ' or up on a/an ' + gets.chomp + '. A good vacation place is one where you can ride ' + gets.chomp + '
+  or play ' + gets.chomp + ' or go hunting for ' + gets.chomp + ' . I like to spend my time ' + gets.chomp + '______ or ' + gets.chomp + '______.
+    When parents go on a vacation, they spend their time eating three ' + gets.chomp + ' a day, and fathers play golf, and mothers sit around ' + gets.chomp + '______. Last summer, my little brother fell in a/an ' + gets.chomp + ' and got poison ' + gets.chomp + ' all
+  over his ' + gets.chomp + '______. My family is going to go to (the)
+  ' + gets.chomp + ', and I will practice ' + gets.chomp + '______. Parents
+  need vacations more than kids because parents are always very
+  ' + gets.chomp + ' and because they have to work ' + gets.chomp + '
+  hours every day all year making enough ' + gets.chomp + ' to pay
+  for the vacation.'
+
+end
+mad_libs
+
+# for each do
+#   # sentence.gsub ('__________________', gets.chomp)
+# '  A vacation is when you take a trip to some gets.chomp place'\n
+# end
+
+# mad_lib_line('A vacation is when you take a trip to some ', gets.chomp, ' place')
+#  mad_lib_line('with your ', gets.chomp, ' family. Usually you go to some place')
+#  mad_lib_line('hat is near a/an  ', gets.chomp, ' or up on a/an')
+#  mad_lib_line(' ', gets.chomp, ' ')
+
+
+# A vacation is when you take a trip to some gets.chomp place
+# with your __________________ family. Usually you go to some place
+# that is near a/an __________________ or up on a/an __________________.
+#   A good vacation place is one where you can ride __________________
+# or play __________________ or go hunting for __________________ . I like to spend my time ________________________ or ________________________.
+#   When parents go on a vacation, they spend their time eating three __________________ a day, and fathers play golf, and mothers sit around ________________________. Last summer, my little brother fell in a/an __________________ and got poison __________________ all
+# over his________________________. My family is going to go to (the)
+# __________________, and I will practice ________________________. Parents
+# need vacations more than kids because parents are always very
+# __________________ and because they have to work__________________
+# hours every day all year making enough __________________ to pay
+# for the vacation.
