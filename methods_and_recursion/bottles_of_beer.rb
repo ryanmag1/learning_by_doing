bc6d427394658c1f0765e8b191bb0b0a32d404f5
Ryan Magee <ryanmag1@yahoo.com>
2016-03-02 07:51:11 -0700

    Correct syntax in line: 1 bottle of beer
    [#114840931]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -11,7 +11,7 @@ puts
 end
 
 puts '2 bottles of beer on the wall, 2 bottles of beer.'
-puts 'Take one down and pass it around, 1 more bottle of beer on the wall.'
+puts 'Take one down and pass it around, 1 bottle of beer on the wall.'
 puts
 puts '1 bottle of beer on the wall, 1 bottle of beer.'
 puts 'Take one down and pass it around, no more bottles of beer on the wall.'
