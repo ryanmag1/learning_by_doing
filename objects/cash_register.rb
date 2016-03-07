aa7fd56643b0060d064ef328ba2e98f3ddb1fbde
Ryan Magee <ryanmag1@yahoo.com>
2016-03-06 18:42:51 -0700

    Configured money strings with sprintf to force 2 decimal positions
    [#114616037]

--- objects/cash_register.rb
+++ objects/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
   end
 
   def total
-    puts "register.total $#{@subtotal.round(2)}"
+    puts "register.total $#{sprintf("%.2f",@subtotal.round(2))}"
   end
 
   def purchase(purchase_amount)
@@ -19,29 +19,32 @@ class CashRegister
 
   def pay(amount_tendered)
     @new_subtotal = @subtotal
-    while @amount_tender == amount_tendered
-      if @amount_tender <= @subtotal
-        @subtotal = @new_subtotal.to_f - @amount_tender.to_f
-        print "register.pay: $#{@amount_tender}"
-      end
-      # Set new variable last_subtotal to pass to calculate change method
-      @last_subtotal = @new_subtotal
-      @new_subtotal = @new_subtotal.to_f - @amount_tender.to_f
-      if @new_subtotal >= 0.00
-        puts "  Your new total is $#{@new_subtotal.round(2)}"
-      else
-        @subtotal = 0.00
-        break
-      end
+    @amount_tender = amount_tendered
+    #   while @amount_tender == amount_tendered
+    if @amount_tender <= @subtotal
+      @subtotal = @new_subtotal.to_f - @amount_tender.to_f
+      print "register.pay: $#{sprintf("%.2f",@amount_tender)}"
     end
+    # Set new variable last_subtotal to pass to calculate change method
+    @last_subtotal = @new_subtotal
+    @new_subtotal = @new_subtotal.to_f - @amount_tender.to_f
+    if @new_subtotal >= 0.00
+      puts
+      puts "  Your new total is $#{sprintf("%.2f",@new_subtotal.round(2))}"
+    else
+      @subtotal = 0.00
+      #   break
+    end
+#  end
   end
 
   def calculate_change
     change_due = @amount_tender.to_f - @last_subtotal.to_f
-    puts "register.pay: $#{@amount_tender}
-  Your change is: $#{change_due.round(2)}"
+    puts "register.pay: $#{sprintf("%.2f",@amount_tender)}
+  Your change is: $#{sprintf("%.2f",change_due.round(2))}"
     puts 'Thank you and have a nice day!'
   end
+
 end
 
 def start_transaction_1
