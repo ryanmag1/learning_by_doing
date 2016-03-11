027e0b92888401830dee2f352b367ebf0a584de6
Ryan Magee <ryanmag1@yahoo.com>
2016-03-11 16:44:14 -0700

    Created arrays for fizz etc and 1-100.  Print of array give proper fizzbuzz results
    [#114616273]

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -1,18 +1,42 @@
 #!/usr/bin/env ruby
 
-(1..100).each do |number|
-  fizz = (number % 3 == 0)
-  buzz = (number % 5 == 0)
+@numbers = Array(1..100)
+@fizz_buzz_array = ['Fizz', 'Buzz', 'FizzBuzz']
 
-    puts case
-           when fizz && buzz then
-             'FizzBuzz'
-           when fizz then
-             'Fizz'
-           when buzz then
-             'Buzz'
-           else
-             number
-         end
-    end
+@numbers.map! {|number| number % 5 == 0 && number % 3 == 0 ? @fizz_buzz_array[2]: number }.flatten!
+@numbers.map! {|number| number % 3 == 0 ? @fizz_buzz_array[0]: number }.flatten!
+@numbers.map! {|number| number % 5 == 0 ? @fizz_buzz_array[1] : number }.flatten!
+
+puts @numbers
+
+#@number_array.each do |number|
+#  fizz = (number % 3 == 0)
+#  buzz = (number % 5 == 0)
+
+#    puts case
+#           when fizz && buzz then
+#             'FizzBuzz'
+#           when fizz then
+#             'Fizz'
+#           when buzz then
+#             'Buzz'
+#           else
+#             number
+#         end
+#    end
+# (1..100).each do |number|
+#  fizz = (number % 3 == 0)
+#  buzz = (number % 5 == 0)
+#
+#    puts case
+#           when fizz && buzz then
+#             'FizzBuzz'
+#           when fizz then
+#             'Fizz'
+#           when buzz then
+#             'Buzz'
+#           else
+#             number
+#         end
+#    end
 
