55b3f14242bbc31f3e44d9d1352eb2d82ba6368d
Ryan Magee <ryanmag1@yahoo.com>
2016-03-26 16:04:27 -0600

    Reworked to use a case statement so that numbers array was only mapped through one time.
    [#114616273]

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -3,40 +3,17 @@
 @numbers = Array(1..100)
 @fizz_buzz_array = ['Fizz', 'Buzz', 'FizzBuzz']
 
-@numbers.map! { |number| number % 5 == 0 && number % 3 == 0 ? @fizz_buzz_array[2] : number }.flatten!
-@numbers.map! { |number| number % 3 == 0 ? @fizz_buzz_array[0] : number }.flatten!
-@numbers.map! { |number| number % 5 == 0 ? @fizz_buzz_array[1] : number }.flatten!
+@numbers.map! do |number|
+  case
+    when number % 5 == 0 && number % 3 == 0 then
+      @fizz_buzz_array[2]
+    when number % 3 == 0 then
+      @fizz_buzz_array[0]
+    when number % 5 == 0 then
+      @fizz_buzz_array[1]
+    else
+      number
+  end
+end
 
 puts @numbers
-
-#@number_array.each do |number|
-#  fizz = (number % 3 == 0)
-#  buzz = (number % 5 == 0)
-
-#    puts case
-#           when fizz && buzz then
-#             'FizzBuzz'
-#           when fizz then
-#             'Fizz'
-#           when buzz then
-#             'Buzz'
-#           else
-#             number
-#         end
-#    end
-# (1..100).each do |number|
-#  fizz = (number % 3 == 0)
-#  buzz = (number % 5 == 0)
-#
-#    puts case
-#           when fizz && buzz then
-#             'FizzBuzz'
-#           when fizz then
-#             'Fizz'
-#           when buzz then
-#             'Buzz'
-#           else
-#             number
-#         end
-#    end
-
