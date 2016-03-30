09c628761140ef8a4f61f8724ffee6084bc4ddf4
Ryan Magee <ryanmag1@yahoo.com>
2016-03-30 09:58:19 -0600

    Removed unnessary array
    [#114616273]

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -1,19 +1,19 @@
 #!/usr/bin/env ruby
 
 @numbers = Array(1..100)
-@fizz_buzz_array = ['Fizz', 'Buzz', 'FizzBuzz']
-
 @numbers.map! do |number|
+
   case
-    when number % 5 == 0 && number % 3 == 0 then
-      @fizz_buzz_array[2]
-    when number % 3 == 0 then
-      @fizz_buzz_array[0]
-    when number % 5 == 0 then
-      @fizz_buzz_array[1]
+    when number % 5 == 0 && number % 3 == 0
+      'FizzBuzz'
+    when number % 3 == 0
+      'Fizz'
+    when number % 5 == 0
+      'Buzz'
     else
       number
   end
 end
 
 puts @numbers
+
